860a6ca698b6f4279dbaced7c259a8bdc44a5e86
sheikm01 <mohamed.sheikh@bbc.co.uk>
2019-05-14 10:50:11 +0100

    latest tag

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -129,7 +129,6 @@ class Schedule extends React.Component {
   handleClick(startTime){
     
     this.setState({index : null})
-    alert('hit')
     for(let i = 0; i < videos.length; i++) {
       if(videos[i].props.startTime === startTime && videos[i].props.flag !== true){
       
