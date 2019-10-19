ce4acf58db64caf55d1d634aa768a63a645c331e
mo-sheikh <mohamed.sheikh@bbc.co.uk>
2019-10-19 22:38:04 +0100

    conflicts resolved

--- src/SingleSchedule/SingleSchedule.jsx
+++ src/SingleSchedule/SingleSchedule.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
 import moment from 'moment';
 import Arrow from '@material-ui/icons/ArrowRightAlt';
@@ -54,60 +53,3 @@ return (
 }
 }
 export default SingleSchedule;
-=======
-import React, { Fragment } from 'react';
-import moment from 'moment';
-import Arrow from '@material-ui/icons/ArrowRightAlt';
-
-var status;
-class SingleSchedule extends React.Component {
-
-    timeFormat(){
-    if(moment.duration(this.props.duration)._data.seconds.toString().length === 1){
-        return "0" + (moment.duration(this.props.duration)._data.seconds)
-      } else {
-        return (moment.duration(this.props.duration)._data.seconds)
-      }
-  }
-
-  componentDidUpdate(prevProps){  
-    if(prevProps.flag !== this.props.flag){
-         this.forceUpdate();
-    }
-
-
-  }
-    render() {
-        if(this.props.prev === 1){
-            var additional = null;
-       }else if (this.props.prev !== 1 && this.props.style !== undefined) {
-            additional = 
-            <td><button className="mini ui button" onClick={() => this.props.deleteItem(this.props.id)}><i class="trash alternate outline icon"></i></button>
-            </td>;
-        }else{
-            additional = 
-            <td><button className="mini ui button" onClick={() => this.props.deleteItem(this.props.id)}>Delete</button> 
-            </td>;
-        }
-        this.props.flag === true ? status = <Arrow/> : status =  null;
-  
-return (
-    <Fragment className = {this.props.live}>
-      
-        <tr className = {this.props.live}>
-        <td onClick={() => this.props.fetchTime(this.props.id, status)}> {status}</td>
-        <td className="collapsing" className = {this.props.style}>
-        <input type="checkbox"/> <label></label>
-        </td>
-        <td className = {this.props.style}>{this.props.startTime}</td>
-        <td className = {this.props.style}>{this.props.title}</td>
-        <td className = {this.props.style}>{moment.duration(this.props.duration)._data.minutes}:{this.timeFormat()}</td>
-        {additional}
-    </tr>
-    
-    </Fragment>
-);
-}
-}
-export default SingleSchedule;
->>>>>>> 405ad2cf621e295cb9fdbfd2b5ef57b6ba084c85
