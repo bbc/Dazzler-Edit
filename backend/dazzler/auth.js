0b55e1ef7a0d720c8158d2623878467d3a131892
mo-sheikh <mohamed.sheikh@bbc.co.uk>
2019-10-24 13:16:45 +0100

    fixed serverjs dependency issue

--- backend/dazzler/auth.js
+++ backend/dazzler/auth.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 
 function auth(email) {
-  const auth = fs.readFileSync("/usr/lib/dazzler/auth.txt");
+  if(process.env.AUTHORISED_USERS){
+    const auth = fs.readFileSync("/usr/lib/dazzler/auth.txt");
+    return auth.includes("," + email + ",");
+  }else{
+  const auth = fs.readFileSync("auth.txt");
   return auth.includes("," + email + ",");
+}
 }
 
 module.exports = auth;
