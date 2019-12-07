474344fc4ac4d0b800fe8b6e54762ed9d14764c7
jcable <julian.cable@yahoo.com>
2019-12-07 17:29:09 +0000

    log auth

--- backend/dazzler/auth.js
+++ backend/dazzler/auth.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 function auth(email) {
   if(process.env.AUTHORISED_USERS){
     const auth = ","+process.env.AUTHORISED_USERS.trim()+",";
+    console.log('auth', auth, email);
     return auth.includes(`,${email.trim()},`);
   }else{
   return true; // allow saving in the local environment
