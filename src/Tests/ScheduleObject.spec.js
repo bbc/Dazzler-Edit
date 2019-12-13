14eec4e6e8dbb470251645218f0b5b19e9c9fee0
jcable <julian.cable@yahoo.com>
2019-12-13 10:16:47 +0000

    add test

--- src/Tests/ScheduleObject.spec.js
+++ src/Tests/ScheduleObject.spec.js
@@ -5,10 +5,9 @@
   describe("ScheduleObject", () => {
 
   test("Should add item at the chosen index", () => {
-    debugger;
     let myScheduleObject = new ScheduleObject(
       "bbc_marathi_tv",
-      moment().format("YYYY-MM-DD")
+      "2019-12-08",
     );
     myScheduleObject.addFloating(1, [
       { duration: "PT5M", insertionType: "midLoop" }
@@ -36,10 +35,9 @@
   });
 
   test("Should add item with a gap before live", () => {
-    debugger;
     let myScheduleObject = new ScheduleObject(
       "bbc_marathi_tv",
-      moment().format("YYYY-MM-DD"),
+      "2019-12-08",
       [
         {
           title: "Dummy start",
@@ -156,6 +154,167 @@
     expect(myScheduleObject.items[5].insertionType).toBe('gap');
     expect(myScheduleObject.items[6].insertionType).toBe('sentinel');
     expect(myScheduleObject.items.length).toBe(expected.length);
+    expect(myScheduleObject.items).toMatchObject(expected);    
+  });
+
+  test("Should add items to empty schedule with unique start times", () => {
+    let myScheduleObject = new ScheduleObject("bbc_marathi_tv", "2019-12-08");
+    myScheduleObject.addFloating(1, [
+      { 
+        title: "test",
+        live: false,
+        duration: "PT5M", 
+        insertionType: "loopStart" 
+      },
+      { 
+        title: "test",
+        live: false,
+        duration: "PT5M", 
+        insertionType: "midLoop" 
+      },
+      { 
+        title: "test",
+        live: false,
+        duration: "PT5M", 
+        insertionType: "loopEnd" 
+      }
+    ]);
+
+    const expected = [
+      {
+        title: "Dummy start",
+        duration: "PT0S",
+        //startTime: moment("2019-12-08T00:00:00Z"),
+        live: false,
+        insertionType: "sentinel"
+      },
+      {
+        title: "test",
+        //startTime: moment("2019-12-08T00:00:00Z"),
+        duration: "PT5M",
+        live: false,
+        insertionType: "loopStart"
+      },
+      {
+        title: "test",
+        //startTime: moment("2019-12-08T00:05:00Z"),
+        duration: "PT5M",
+        live: false,
+        insertionType: "midLoop"
+      },
+      {
+        title: "test",
+        //startTime: moment("2019-12-08T00:10:00Z"),
+        duration: "PT5M",
+        live: false,
+        insertionType: "loopEnd"
+      },
+      {
+        title: "gap",
+        //startTime: moment("2019-12-08T00:15:00Z"),
+        duration: "PT23H45M",
+        live: false,
+        insertionType: "gap"
+      },
+      {
+        title: "Dummy end",
+        duration: "PT0S",
+        //startTime: moment("2019-12-09T00:00:00Z"),
+        live: false,
+        insertionType: "sentinel"
+      }
+    ];
+
+    //expect the new item to be added
+
+    const items = myScheduleObject.items;
+
+    expect(items[0].insertionType).toBe('sentinel');
+    expect(items[1].insertionType).toBe('loopStart');
+    expect(items[2].insertionType).toBe('midLoop');
+    expect(items[3].insertionType).toBe('loopEnd');
+    expect(items[4].insertionType).toBe('gap');
+    expect(items[5].insertionType).toBe('sentinel');
+    expect(items.length).toBe(expected.length);
+
+    expect(items[1].startTime.utc().format()).toBe('2019-12-08T00:00:00Z');
+    expect(items[2].startTime.utc().format()).toBe('2019-12-08T00:05:00Z');
+    expect(items[3].startTime.utc().format()).toBe('2019-12-08T00:10:00Z');
+    expect(items[4].startTime.utc().format()).toBe('2019-12-08T00:15:00Z');
+
+    expect(myScheduleObject.items).toMatchObject(expected);    
+  });
+
+  test("Should add items to non-empty schedule with unique start times", () => {
+    let myScheduleObject = new ScheduleObject("bbc_marathi_tv", "2019-12-08");
+    const test_item = {
+      title: "test",
+      live: false,
+      duration: "PT1M", 
+      insertionType: ""
+    };
+    myScheduleObject.addFloating(1, test_item);
+    const test_item2 = {
+      title: "test 2",
+      live: false,
+      duration: "PT1M", 
+      insertionType: ""
+    };
+    myScheduleObject.addFloating(2, test_item2);
+
+    const expected = [
+      {
+        title: "Dummy start",
+        duration: "PT0S",
+        //startTime: moment("2019-12-08T00:00:00Z"),
+        live: false,
+        insertionType: "sentinel"
+      },
+      {
+        title: "test",
+        //startTime: moment("2019-12-08T00:00:00Z"),
+        duration: "PT1M",
+        live: false,
+        insertionType: ""
+      },
+      {
+        title: "test 2",
+        //startTime: moment("2019-12-08T00:01:00Z"),
+        duration: "PT1M",
+        live: false,
+        insertionType: ""
+      },
+      {
+        title: "gap",
+        //startTime: moment("2019-12-08T00:02:00Z"),
+        duration: "PT23H58M",
+        live: false,
+        insertionType: "gap"
+      },
+      {
+        title: "Dummy end",
+        duration: "PT0S",
+        //startTime: moment("2019-12-09T00:00:00Z"),
+        live: false,
+        insertionType: "sentinel"
+      }
+    ];
+
+    //expect the new item to be added
+
+    const items = myScheduleObject.items;
+
+    expect(items[0].insertionType).toBe('sentinel');
+    expect(items[1].insertionType).toBe('');
+    expect(items[2].insertionType).toBe('');
+    expect(items[3].insertionType).toBe('gap');
+    expect(items[4].insertionType).toBe('sentinel');
+    expect(items.length).toBe(expected.length);
+
+    expect(items[1].startTime.utc().format()).toBe('2019-12-08T00:00:00Z');
+    expect(items[2].startTime.utc().format()).toBe('2019-12-08T00:01:00Z');
+    expect(items[3].startTime.utc().format()).toBe('2019-12-08T00:02:00Z');
+
     expect(myScheduleObject.items).toMatchObject(expected);    
   });
 
