e21299e6f9b549c101de95405d70ed6fb4f3e939
jcable <julian.cable@yahoo.com>
2020-01-10 06:21:47 +0000

    DAZZLER-85

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -164,7 +164,8 @@ class Editor extends React.Component {
   handleAddLive(item) {
     const items = this.state.schedule.items;
     for(let i=0; i<items.length; i++) {
-      if(item.startTime.isSame(items[i].startTime)) {
+      if(item[i].insertionType !== 'gap' && item.startTime.isSame(items[i].startTime)) {
+        console.log('silently ignoring add');
         return; // don't allow adding at same point twice
       }
     }
