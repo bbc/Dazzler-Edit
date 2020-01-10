18df1001e064abaeb1401b908cbe54c0d31f622b
jcable <julian.cable@yahoo.com>
2020-01-10 11:03:13 +0000

    DAZZLER-94 for testing

--- backend/dazzler/pips.js
+++ backend/dazzler/pips.js
@@ -2,8 +2,10 @@ const axios = require("axios");
 const fs = require("fs");
 const https = require("https");
 const parseString = require("xml2js").parseString;
+const xml2js = require('xml2js-es6-promise');
 
-const uri = 'https://api.live.bbc.co.uk/pips/api/v1';
+const base_uri = 'https://api.live.bbc.co.uk/pips';
+const uri = base_uri+'/api/v1';
 
 async function xml2json(xml) {
     return new Promise((resolve, reject) => {
@@ -88,56 +90,32 @@ async function clearCollection(pid) {
       passphrase: process.env.PASSPHRASE,
       headers: { "Content-Type": "text/xml" }
     };
-  
-    return await axios.post(
-      `${uri}/${object_type}/`,
-      data,
-      config
-    );
+    try {
+      return await axios.post(
+        `${uri}/${object_type}/`,
+        data,
+        config
+      );  
+    } catch(e) {
+      console.log(e);
+      // TODO fix error handling
+      return null;
+    }
   }
-  
-  function postTVA(data, res) {
-    const options = {
-      path: "/pips/import/tva/",
-      host: "api.live.bbc.co.uk",
-      method: "POST",
+
+  async function postTVA(data) {
+    var config = {
       key: fs.readFileSync(process.env.KEY),
       cert: fs.readFileSync(process.env.CERT),
       passphrase: process.env.PASSPHRASE,
-      headers: {
-        "Content-Type": "application/xml",
-        "Content-Length": Buffer.byteLength(data)
-      }
+      headers: { "Content-Type": "text/xml" }
     };
-    options.agent = new https.Agent(options);
+  
     try {
-      const req = https.request(options, function(post_res) {
-        let body = "";
-        post_res.setEncoding("utf8");
-        post_res.on("data", chunk => {
-          body += chunk;
-        });
-        post_res.on("end", () => {
-          try {
-            parseString(body, function(err, result) {
-              if (err) {
-                res.status(404).send(err);
-              } else {
-                res.json(result);
-              }
-            });
-          } catch (e) {
-            res.status(404).send(e);
-          }
-        });
-      });
-      // post the data
-      req.write(data);
-      req.end();  
+      const r = await axios.post(base_uri+'/import/tva', data, config);
+      return await xml2js(r.data);
     } catch(e) {
-      console.log(e);
-      // assume cert error
-      res.status(401).send(e);
+      return e;
     }
   }
 
