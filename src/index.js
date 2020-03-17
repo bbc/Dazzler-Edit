0dbf8293becf5d4e2411fc4b81fcea516b939106
jcable <julian.cable@yahoo.com>
2020-03-17 22:59:12 +0000

    wip

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Editor from "./Editor/Editor";
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Editor />, document.querySelector("#root"));
-
-//serviceWorker.unregister();
-//serviceWorker.register();
