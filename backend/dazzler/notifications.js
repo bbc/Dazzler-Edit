4b834ad333e3b1e8c27224e6ecf3494eca89eb7b
jcable <julian.cable@yahoo.com>
2020-03-17 19:22:55 +0000

    make work if environment not set

--- backend/dazzler/notifications.js
+++ backend/dazzler/notifications.js
@@ -6,7 +6,7 @@ const s3 = new aws.S3({ apiVersion: "2006-03-01" });
 async function getSubscriptions(sid) {
     try {
         const s = await s3.getObject({
-            Bucket: process.env.BUCKET,
+            Bucket: process.env.BUCKET || 'ws-dazzler-assets-test',
             Key: `${sid}/subscriptions`
         }).promise();
         return JSON.parse(s.Body.toString("utf-8"));            
@@ -17,7 +17,7 @@ async function getSubscriptions(sid) {
 
 async function putSubscriptions(sid, subscriptions) {
     return s3.putObject({
-        Bucket: process.env.BUCKET,
+        Bucket: process.env.BUCKET || 'ws-dazzler-assets-test',
         Key: `${sid}/subscriptions`,
         Body: JSON.stringify(subscriptions),
         ContentType: 'application/json'
@@ -27,11 +27,16 @@ async function putSubscriptions(sid, subscriptions) {
 async function addSubscription(sid, subscription) {
     console.log('addSubscription', sid, subscription);
     const subscriptions = await getSubscriptions(sid);
-    if (subscriptions.includes(subscription)) {
-        return;
-    }
-    subscriptions.push(subscription);
-    return putSubscriptions(sid, subscriptions);
+    const s = {};
+    subscriptions.forEach((sub) => {
+        s[sub.endpoint] = sub;
+    });
+    s[subscription.endpoint] = subscription;
+    const newsubs = [];
+    Object.keys(s).forEach((key) => {
+        newsubs.push(s[key]);
+    });
+    return putSubscriptions(sid, newsubs);
 }
 
 async function removeSubscription(sid, subscription) {
