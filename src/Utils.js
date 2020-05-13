b746f0ea7832fc227a230d198b91a7367b45fca4
mo-sheikh <mohamed.sheikh@bbc.co.uk>
2020-05-13 14:56:28 +0100

    added helper function

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 class Utils {
-  static episodeInfo = item => {
+  static episodeInfo = (item) => {
     try {
       if (!item.availability.actual_start) {
         item.status = "unavailable";
@@ -8,7 +8,9 @@ class Utils {
           "grey",
           `Expected ${moment(item.availability.expected_start).format(
             "DD/MM/YY"
-          )} at ${moment(item.availability.expected_start).format("HH:MM:SS")} `
+          )} at ${moment(item.availability.expected_start).format(
+            "HH:MM:SS"
+          )} `,
         ];
       } else {
         item.status = "";
@@ -20,3 +22,21 @@ class Utils {
 
 export const episodeInfo = Utils.episodeInfo;
 export default Utils;
+
+class Group {
+  constructor(obj) {
+    this.data = [];
+    if (typeof obj[Symbol.iterator] === "function") {
+      for (let x of obj) {
+        if (x.asset.vpid && !JSON.stringify(this.data).includes(x.asset.vpid)) {
+          this.data.push(x);
+        }
+      }
+    }
+  }
+
+  get size() {
+    return this.data.length;
+  }
+}
+export const groupSet = Group;
