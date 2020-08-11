1ada5a3b802f540a75a0765bb93680a00f9355e3
Mohamed Sheikh <mohamed.sheikh@bbc.co.uk>
2020-08-11 13:08:29 +0100

    wip

--- src/Tests/ScheduleDao.spec.js
+++ src/Tests/ScheduleDao.spec.js
@@ -1,7 +1,4 @@
-import configv2 from "./templates/ScheduleObjectItems";
 import axios from "axios";
-
-
 import {
   fetchSchedule,
   fetchSchedulev2,
@@ -10,27 +7,27 @@ import {
   saveScheduleV2,
   getLanguages,
 } from "../ScheduleDao/ScheduleDao";
-
+import { configV2 } from "./templates/ScheduleDaoItems";
+import { config } from "aws-sdk";
+jest.mock("axios");
 describe("ScheduleDao", () => {
-  let languageList;
-  let configObj;
-  let returnedObject = {
-    data: configV2
-  }
+  test("testing getLanguages fetches the list of languages", (done) => {
+    let languageList;
+    let configObj;
 
-  test("Should fetch all available langauges", done => {
-    jest.mock('axios');
-    axios.get.mockResolvedValue(returnedObject);
-    function callback(response){
+    let obj = {
+      data: configV2,
+    };
+
+    axios.get.mockResolvedValue(obj);
+    function callBack(response) {
       languageList = Object.keys(response);
       configObj = response;
-      expect(languageList).toBe(["s"]);
+      expect(languageList).toStrictEqual(["Hindi", "Marathi", "Swahili"]);
       done();
     }
-    getLanguages(response)
+    let fetchLanguages = getLanguages(callBack);
 
-  })
-
-    
-    
-    
+    console.log("bang");
+  });
+});
