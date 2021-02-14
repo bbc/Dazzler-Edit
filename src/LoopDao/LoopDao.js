18b4adbf2edae0c202d0ece5990781120255b22f
Mohamed Sheikh <mohamed.sheikh@bbc.co.uk>
2021-02-14 22:34:49 +0000

    emergency playlist using v2

--- src/LoopDao/LoopDao.js
+++ src/LoopDao/LoopDao.js
@@ -3,16 +3,16 @@ const URLPrefix =
   process.env.NODE_ENV === "development" ? "http://localhost:8080" : "";
 class LoopDao {
   static backupPlaylist(loopItems, sid, cb) {
-    const url = `${URLPrefix}/api/v1/loop`;
+    const url = `${URLPrefix}/api/v2/loop`;
     axios({
       method: "post",
       url: url + `?sid=${sid}`,
-      data: loopItems
+      data: loopItems,
     })
-      .then(response => {
+      .then((response) => {
         cb();
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         console.log(error);
       });
